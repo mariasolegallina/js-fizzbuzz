6224efa92a2fc4a073cabc257cec817fa4bbc601
mariasolegallina <gallinamariasole@gmail.com>
2023-10-08 13:42:07 +0200

    add - if, else...

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 for (let i = 1; i < 101; i++) {
     const fizz = i % 3
     const buzz = i % 5
+    
+    if (fizz === 0 && buzz === 0) {
+        console.log("FizzBuzz", i)
+    } else if (fizz === 0 && buzz != 0) {
+        console.log("Fizz", i)
+    } else if (buzz === 0 && fizz != 0) {
+        console.log("Buzz", i)
+    }
 }
