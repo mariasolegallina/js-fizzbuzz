a8020533c3da477c65e62e4d8ec718812ba3d936
mariasolegallina <gallinamariasole@gmail.com>
2023-10-08 21:50:44 +0200

    add - html + css

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ for (let i = 1; i < 101; i++) {
     const buzz = i % 5
     
     if (fizz === 0 && buzz === 0) {
-        console.log("FizzBuzz", i)
+        console.log("FizzBuzz")
     } else if (fizz === 0 && buzz != 0) {
-        console.log("Fizz", i)
+        console.log("Fizz")
     } else if (buzz === 0 && fizz != 0) {
-        console.log("Buzz", i)
+        console.log("Buzz")
+    } else {
+        console.log(i)
     }
 }
